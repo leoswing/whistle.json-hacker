f1adcf33c3bbcdd6c310eb3671878c72ae2b9205
leoswing <1160922634@qq.com>
2024-09-09 18:46:32 +0800

    feat: remove antd related dependencies and antd components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-// import RequestRule from './components/RuleEditor';
-
-import './App.css';
-
 function App() {
   return (
     <>
-      {/* <RequestRule mode='create' /> */}
       <h3>Whistle plugin build with React for json hacker</h3>
     </>
   );
